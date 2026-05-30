71a351b51d55d6877c36abc96622aa910bf8d57b
Timo Wyszynski <timo.wyszynski@gmail.com>
2026-05-30 19:55:17 +0200

    Added the functionality of splitting ndes based on images and links.

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,4 +1,5 @@
 from textnode import TextNode, TextType
+import re
 
 
 def split_nodes_delimiter(
@@ -23,4 +24,67 @@ def split_nodes_delimiter(
         new_nodes.extend(split_nodes)
     return new_nodes
 
+def extract_markdown_images(text):
+    matches = re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+    return matches
 
+def extract_markdown_links(text):
+    matches = re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+    return matches
+
+def split_nodes_image(old_nodes: list[TextNode]) -> list[TextNode]:
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+        matches = extract_markdown_images(old_node.text)
+        if not matches:
+            new_nodes.append(old_node)
+            continue
+        split_nodes = []
+        remaining_text = old_node.text
+        for (alt, url) in matches:
+            sections = remaining_text.split(f"![{alt}]({url})", 1)
+            left = sections[0]
+            remaining_text = sections[1]
+            if left:
+                split_nodes.append(TextNode(left, TextType.TEXT))
+            split_nodes.append(TextNode(alt, TextType.IMAGE, url))
+        if remaining_text:
+            split_nodes.append(TextNode(remaining_text, TextType.TEXT))
+        new_nodes.extend(split_nodes)
+    return new_nodes
+
+def split_nodes_link(old_nodes: list[TextNode]) -> list[TextNode]:
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+        matches = extract_markdown_links(old_node.text)
+        if not matches:
+            new_nodes.append(old_node)
+            continue
+        split_nodes = []
+        remaining_text = old_node.text
+        for (alt, url) in matches:
+            sections = remaining_text.split(f"[{alt}]({url})", 1)
+            left = sections[0]
+            remaining_text = sections[1]
+            if left:
+                split_nodes.append(TextNode(left, TextType.TEXT))
+            split_nodes.append(TextNode(alt, TextType.LINK, url))
+        if remaining_text:
+            split_nodes.append(TextNode(remaining_text, TextType.TEXT))
+        new_nodes.extend(split_nodes)
+    return new_nodes
+
+def text_to_textnodes(text):
+    nodes = [TextNode(text, TextType.TEXT)]
+    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    return nodes
